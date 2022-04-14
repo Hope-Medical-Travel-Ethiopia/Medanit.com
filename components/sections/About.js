7856cb1118df3ccca566bcbb96b95d0a29e30fa9
Samuel-kedir <samuel.datros@gmail.com>
2022-04-14 15:24:54 +0300

    conditional render for description about providers to avoid null

--- components/sections/About.js
+++ components/sections/About.js
@@ -3,11 +3,11 @@ const About = ({ content, description }) => {
     <section className="about">
       <div className="card bg-white drop-shadow-lg rounded-lg md:px-10 px-5 py-6">
         <h1 className="text-blue-500 tracking-wider text-lg font-bold mb-3">
-          {content
-            ? content.profile.description
-            : "Description"}
+          {content ? content.profile.description : "Description"}
         </h1>
-        <p className="text-sm leading-normal">{description}</p>
+        {description && description != "null" && (
+          <p className="text-sm leading-normal">{description}</p>
+        )}
       </div>
     </section>
   );
